f7a69ae5f1dc736705334479371972084db89378
Gunther Klein <gunther.klein@f24.com>
2018-11-14 14:57:10 +0100

    added missing @Override annotation

--- src/main/java/org/abstractj/kalium/encoders/Raw.java
+++ src/main/java/org/abstractj/kalium/encoders/Raw.java
@@ -18,12 +18,13 @@ package org.abstractj.kalium.encoders;
 
 public class Raw implements Encoder {
 
-    public byte[] decode(final String data) {
-        return data != null ? data.getBytes(CHARSET) : null;
-    }
+	@Override
+	public byte[] decode(final String data) {
+		return data != null ? data.getBytes(CHARSET) : null;
+	}
 
-    @Override
-    public String encode(byte[] data) {
-        return data != null ? new String(data, CHARSET) : null;
-    }
+	@Override
+	public String encode(byte[] data) {
+		return data != null ? new String(data, CHARSET) : null;
+	}
 }
